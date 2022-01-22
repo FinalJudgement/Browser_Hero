256459b03b74dd64579a1edac3f0a4d190e3b956
Carlos Silva <silvacj90@gmail.com>
2022-01-21 19:36:01 -0500

    audio test 2

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var bleep = new Audio();
 bleep.src = "beep.mp3";
+
+var isChrome =
+  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome) {
+  $("#iframeAudio").remove();
+} else {
+  $("#playAudio").remove(); // just to make sure that it will not have 2x audio in the background
+}
